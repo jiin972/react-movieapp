0a8c2c3a8fd6e19e8ba9929468eb9687756805ff
seoungjin lim <30948247+jiin972@users.noreply.github.com>
2025-07-03 11:52:32 +0900

    update root

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -12,7 +12,7 @@ function Movie({ id, coverImg, title, year, summary, genres }) {
       </h2>
       <h3 className={styles.movie__year}>{year}</h3>
       <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-      <ul className={styles.movie__genres}>
+      <ul className={styles.movie__gen}>
         {genres &&
           Array.isArray(genres) &&
           genres.map((g) => <li key={g}>{g}</li>)}
